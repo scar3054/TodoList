12705816f48f7ede16e0952f35a9bf6fd397c86b
scar3054 <ciaofef@hotmail.it>
2019-12-27 14:41:53 +0100

    TodoList base

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+
+  aggiornaInput(key,value){
+    this.setState({
+      [key]: value
+    });
+  }
+
+  aggiungiItem(){
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    const list = [...this.state.list];
+
+    list.push(newItem);
+
+    this.setState({
+      list,
+      newItem:""
+    });
+  }
+
+  eliminaItem(id){
+    const list = [...this.state.list];
+
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({list: updatedList});
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <div>
+        Aggiungi un todo!
+        <br/>
+        <input 
+        type = "text" 
+        placeholder = "Scrivi un todo!"
+        value = {this.state.newItem}
+        onChange = {e => this.aggiornaInput("newItem", e.target.value)}
+        />
+        <button onClick={() => this.aggiungiItem()}>
+          Aggiungi
+        </button>
+        <br/>
+        <ul>
+          {this.state.list.map(item => {
+            return(
+              <li key={item.id}>
+                {item.value}
+                <button onClick={() => this.eliminaItem(item.id)}>
+                  Rimuovi
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
+
 export default App;
+
+
+
+
+
